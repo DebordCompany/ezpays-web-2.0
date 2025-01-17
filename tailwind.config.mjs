42df23ae1605692826c7a410438d590d08dfb96e
DebordCompany <debord.info@gmail.com>
2025-01-17 21:40:42 +0100

    style(home): restyling home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      screens: {
+        xs: "330px",
+        sm: "540px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "2000px",
+      },
       colors: {
         black: "#333",
         "soft-gray": "#AFAFAF",
